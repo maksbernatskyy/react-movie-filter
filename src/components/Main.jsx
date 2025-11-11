c6669f8b149cc0f2a919c3c7e7243dc721536c83
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-11 11:33:42 +0100

    add array of movies

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,17 @@
+import { useState, useEffect } from "react"
+
+// Import array of movies
+import movies from "../assets/movies"
+
 export default function Main() {
+    
+    {/* Array of movies */}
+    <movies />
+
     return (
         <>
         <main>
-            
+
         </main>
         </>
     )
