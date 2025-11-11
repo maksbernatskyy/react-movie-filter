5f2071c848e3baae849a156a0e6fa9155dc04e56
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-11 12:47:44 +0100

    add select

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,36 @@ export default function Main() {
     {/* Array of movies */}
     <movies />
     
-    
-       
-    
+    return (
+        <>
+        <main className="py-3">
+            <div className="container">
+
+                {/* Card */}
+                <div className="card">
+
+                    {/* Card Top */}
+                    <div className="card-top">
+                        {/* Form */}
+                        <form>
+                            {/* Select of genres */}
+                            <select className="form-select">
+                                <option defaultValue={0}>Selecte the genre of movie</option>
+                                <option value="1">Fantascienza</option>
+                                <option value="2">Thriller</option>
+                                <option value="3">Romantico</option>
+                                <option value="4">Azione</option>
+                            </select>
+                        </form>
+                    </div>
+
+                    {/* Card Bottom */}
+                    <div className="card-body">
+
+                    </div>
+                </div>
+            </div>
+        </main>
+        </>
+    )
 }
