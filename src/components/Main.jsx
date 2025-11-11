68f51314e6db66e2dec3e2a6ba014849563ba8ee
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-11 11:45:24 +0100

    add select of genres on the card

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,7 +11,30 @@ export default function Main() {
     return (
         <>
         <main>
+            <div className="container">
 
+                {/* Card */}
+                <div className="card">
+
+                    {/* Card Top */}
+                    <div className="card-top">
+                        <form>
+                            <select className="form-select">
+                                <option defaultValue={0}>Select the genre of movie</option>
+                                <option value="1">Fantascienza</option>
+                                <option value="2">Thriller</option>
+                                <option value="3">Romantico</option>
+                                <option value="4">Azione</option>
+                            </select>
+                        </form>
+                    </div>
+
+                    {/* Card Body */}
+                    <div className="card-body">
+
+                    </div>
+                </div>
+            </div>
         </main>
         </>
     )
