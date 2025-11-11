d40e3f546913b20cc062aba8a86a8dc0d7cb8cdb
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-11 11:27:15 +0100

    add folder components with header and main

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <>
+        <header className="p-3">
+            <h1 className="text-uppercase">
+                movies
+            </h1>
+        </header>
+        </>
+    )
+}
